2ea82fbafb04968e20b07c231b914f3a8da1b4dc
Alexandre <= alexandrec@alu.ufc.br>
2025-06-14 16:19:44 +0000

    feat: creating category operations

--- src/domain/category.entity.ts
+++ src/domain/category.entity.ts
@@ -35,6 +35,29 @@ export class Category {
         return new Category(props);
     }
 
+    changeName(name: string): void {
+        this.name = name;
+    }
 
+    changeDescription(description: string): void {
+        this.description = description;
+    }
 
+    activate(){
+        this.isActive = true;
+    }
+
+    deactivate(){
+        this.isActive = false;
+    }
+
+    toJSON() {
+        return {
+            categoryId: this.categoryId,
+            name: this.name,
+            description: this.description,
+            isActive: this.isActive,
+            createdAt: this.createdAt
+        };
+    }
 }
