4804f362d74862bdd34311ecba95578023daf532
Alexandre <= alexandrec@alu.ufc.br>
2025-07-08 14:32:12 +0000

    feat: creat method search in memory

--- src/category/domain/infra/db/category-in-memory.repository.ts
+++ src/category/domain/infra/db/category-in-memory.repository.ts
@@ -1,10 +1,42 @@
+import { SortDirection } from "../../../../shared/domain/repository/search-param";
 import { Uuid } from "../../../../shared/domain/value-object/uuid.vo";
-import { InMemoryRepository } from "../../../../shared/infra/db/in-memory/in-memory.repository";
+import {
+  InMemoryRepository,
+  InMemorySearchRepository,
+} from "../../../../shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../category.entity";
 
-export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid>{
-    
-    getEntity(): new (...args: any[]) => Category {
-       return Category;
+export class CategoryInMemoryRepository extends InMemorySearchRepository<
+  Category,
+  Uuid
+> {
+
+ sortebleFields: string[] = ["name", "createAt"];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: string
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
     }
+
+    return items.filter((i) => {
+      return i.name.value.toLowerCase().includes(filter.toLocaleLowerCase());
+    });
+  }
+
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sortDir: SortDirection | null
+  ) {
+    return sort
+      ? super.applySort(items, sort, sortDir)
+      : super.applySort(items, "createAt", "desc");
+  }
+
+  getEntity(): new (...args: any[]) => Category {
+    return Category;
+  }
 }
--- src/shared/infra/db/in-memory/in-memory.repository.ts
+++ src/shared/infra/db/in-memory/in-memory.repository.ts
@@ -1,6 +1,14 @@
 import { NotFoundException } from "../../../../category/domain/commons/exceptions/not-found.exception";
 import { Entity } from "../../../domain/entity";
-import { IRepository } from "../../../domain/repository/repository.interface";
+import {
+  IRepository,
+  ISearchableRepository,
+} from "../../../domain/repository/repository.interface";
+import {
+  SearchParams,
+  SortDirection,
+} from "../../../domain/repository/search-param";
+import { SearchResult } from "../../../domain/repository/search-results";
 import { ValueObject } from "../../../domain/value-object";
 
 export abstract class InMemoryRepository<
@@ -23,7 +31,7 @@ export abstract class InMemoryRepository<
       item.entityId.equals(entity.entityId)
     );
     if (indexFound === -1) {
-      throw new NotFoundException(entity.entityId,this.getEntity());
+      throw new NotFoundException(entity.entityId, this.getEntity());
     }
     this.items[indexFound] = entity;
   }
@@ -48,4 +56,76 @@ export abstract class InMemoryRepository<
   }
 
   abstract getEntity(): new (...args: any[]) => E;
+}
+
+export abstract class InMemorySearchRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    Filter = string
+  >
+  extends InMemoryRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>
+{
+  sortebleFields: string[] = [];
+
+  async search(props: SearchParams<Filter>): Promise<SearchResult<E>> {
+    
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemSorted = this.applySort(itemsFiltered, props.sort, props.sortDir);
+    const itemPaginated = this.applyPaginate(
+      itemSorted,
+      props.page,
+      props.perPage
+    );
+
+    return new SearchResult({
+      items: itemPaginated,
+      total: itemsFiltered.length,
+      currentPage: props.page,
+      perPage: props.perPage,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: Filter | null
+  ): Promise<E[]>;
+
+  protected applyPaginate(
+    items: E[],
+    page: SearchParams["page"],
+    perPage: SearchParams["perPage"]
+  ) {
+    const start = (page - 1) * perPage;
+    const limit = start + perPage;
+    return items.slice(start, limit);
+  }
+
+  protected applySort(
+    items: E[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+    customGetter?: (sort: string, item: E) => any
+  ) {
+    if (!sort || !this.sortebleFields.includes(sort)) {
+      return items;
+    }
+
+    return [...items].sort((a, b) => {
+      //@ts-ignore
+      const aValue = customGetter ? customGetter(sort, a) : a[sort];
+      //@ts-ignore
+      const bValue = customGetter ? customGetter(sort, b) : b[sort];
+
+      if (aValue < bValue) {
+        return sortDir === "asc" ? -1 : 1;
+      }
+
+      if (aValue > bValue) {
+        return sortDir === "asc" ? 1 : -1;
+      }
+
+      return 0;
+    });
+  }
 }
